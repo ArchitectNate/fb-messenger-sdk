bb6c6d9ead2143f3b0035aec14649c8f17fff016
Nathan Sepulveda <nathans@thephpeffect.com>
2018-07-11 20:47:35 -0700

    adding missing strict_types

--- src/Api/Request.php
+++ src/Api/Request.php
@@ -1,15 +1,9 @@
 <?php
 
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
+declare(strict_types=1);
 
-/**
- *
- * @author natha
- */
+use ArchNate\MessengerSdk\Api;
+
 class Request {
-    //put your code here
+    
 }
